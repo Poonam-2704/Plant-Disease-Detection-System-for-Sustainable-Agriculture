89838cbc537ff064daf9a7396ed6907dbeafcec0
Poonamsuthar <poonams2704@gmail.com>
2025-01-28 20:48:07 +0530

    Update main-checkpoint.py

--- main-checkpoint.py
+++ main-checkpoint.py
@@ -5,39 +5,32 @@ def model_prediction(test_image):
     model = tf.keras.models.load_model("trained_plant_disease_model.keras")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr]) #convert single image to batch
+    input_arr = np.array([input_arr]) 
     predictions = model.predict(input_arr)
-    return np.argmax(predictions) #return index of max element
-
-#Sidebar
+    return np.argmax(predictions) 
+    
 st.sidebar.title("Plant Disease Detection System for Sustainable Agriculture")
 app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
-#app_mode = st.sidebar.selectbox("Select Page",["Home"," ","Disease Recognition"])
 
-# import Image from pillow to open images
 from PIL import Image
 img = Image.open("Diseases1.png")
 
-# display image using streamlit
-# width is used to set the width of an image
 st.image(img)
 
-#Main Page
 if(app_mode=="HOME"):
     st.markdown("<h1 style='text-align: center;'>Plant Disease Detection System for Sustainable Agriculture", unsafe_allow_html=True)
-    
-#Prediction Page
+ 
 elif(app_mode=="DISEASE RECOGNITION"):
     st.header("Plant Disease Detection System for Sustainable Agriculture")
     test_image = st.file_uploader("Choose an Image:")
     if(st.button("Show Image")):
         st.image(test_image,width=4,use_column_width=True)
-    #Predict button
+ 
     if(st.button("Predict")):
         st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
-        #Reading Labels
+ 
         class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
                     'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
                     'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
